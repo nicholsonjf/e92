e47941e28b51f4a840d11736378411032ff8d090
James Nicholson <jnicholson@hsph.harvard.edu>
2021-02-05 04:47:06 +0000

    argc gets number of arguments

--- src/simple-shell/shell.c
+++ src/simple-shell/shell.c
@@ -6,11 +6,45 @@ char shell(void) {
 }
 
 int main(int argc, char** argv) {
-    printf("$");
+    printf("%d %s\n", argc, argv[1]);
     while (1) {
-        char c = fgetc(stdin);
-        if (c == '\n') {
-            break;
+        char linebuf[256];
+        int index = 0;
+        printf("$");
+        while (1) {
+            char c = fgetc(stdin);
+            if (c == '\n') {
+                break;
+            }
+            if (c == ' ' || c == '\t') {
+                linebuf[index] = 0;
+            }
+            else {
+                linebuf[index] = c;
+            }
+            index++;
         }
+        // argv[0] = &linebuf[0]
+        // argv[1] = &linebuf[10]
+        // 0 = whitespace, 1 = word
+        char pctype = 0;
+        int argct = 0;
+        for (int i=0; i<index; i++) {
+            if (pctype == 0) {
+                if (linebuf[i] == 0) {
+                    continue;
+                }
+                else {
+                    argct++;
+                    pctype = 1;
+                }
+            }
+            else {
+                if (linebuf[i] == 0) {
+                    pctype = 0;
+                }
+            }
+        }
+
     }
 }
