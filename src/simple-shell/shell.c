b9e302ba336e88637ae4af9c7d312a2664f0f298
James Nicholson <jnicholson@hsph.harvard.edu>
2021-02-07 11:55:23 -0500

    Improved comments, setting argval

--- src/simple-shell/shell.c
+++ src/simple-shell/shell.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-char shell(void) {
-    char c = fgetc(stdin);
-    return c;
-}
 
 int main(int argc, char** argv) {
     printf("%d %s\n", argc, argv[1]);
     while (1) {
+        // Is a line guaranteed to be < 256 bytes?
         char linebuf[256];
         int index = 0;
         printf("$");
         while (1) {
             char c = fgetc(stdin);
+            // Stop reading characters if we reach a newline.
             if (c == '\n') {
                 break;
             }
+            // Set whitespace to the null terminator.
             if (c == ' ' || c == '\t') {
                 linebuf[index] = 0;
             }
@@ -24,23 +23,36 @@ int main(int argc, char** argv) {
             }
             index++;
         }
+        // Example for how to set argv.
         // argv[0] = &linebuf[0]
         // argv[1] = &linebuf[10]
-        // 0 = whitespace, 1 = word
+
+        // Previous character type.
+        // 0 = whitespace, 1 = character.
         char pctype = 0;
+        // Argument count.
         int argct = 0;
+        char** argval;
+        // Iterate over characters in the line buffer and set argct, argval.
         for (int i=0; i<index; i++) {
+            // If previous character is a space.
             if (pctype == 0) {
+                // If current character is also a space.
                 if (linebuf[i] == 0) {
                     continue;
                 }
+                // If current character is a character.
                 else {
+                    // Increment arg count, set arg pointer, set prev char type to character.
                     argct++;
+                    argval[argct] = &linebuf[i];
                     pctype = 1;
                 }
             }
             else {
+                // If previous character is a character.
                 if (linebuf[i] == 0) {
+                    // Set prev char type to space.
                     pctype = 0;
                 }
             }
