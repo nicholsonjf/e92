03c083f4e2a2e0e2a423f54253c0b730899de91c
James Nicholson <jnicholson@hsph.harvard.edu>
2021-02-22 23:51:39 -0800

    Started best fit algo

--- src/my-malloc/my-malloc.c
+++ src/my-malloc/my-malloc.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 static int malloc_initd = 0;
 
@@ -40,7 +41,6 @@ static struct mem_region *endmymem;
 static void malloc_init(void) {
     uint32_t aspacesize = 128 * (1 << 20);
     void *memory = malloc(aspacesize);
-    void *pid = malloc(sizeof(struct pcb));
     mymem = (struct mem_region*)memory;
     mymem->free = 1;
     mymem->size = aspacesize - sizeof(struct mem_region);
@@ -55,77 +55,36 @@ void *myMalloc(uint32_t size) {
     if (malloc_initd == 0) {
         malloc_init();
     }
+    struct mem_region *best = NULL;
     struct mem_region *current = mymem;
     while (current < endmymem) {
+        // If we're giving them a pointer to the closest double word boundary that is on or
+        // after current->data don't we need to make sure size is lte current->size +
+        // distance to next double word boundary?
         if (current->free == 1 && current->size >= size) {
-            void *mp = current->data + sizeof(dword) - ((uintptr_t)current->data & (sizeof(dword) - 1));
+            if (best == NULL || current < best)
+            {
+                best = current;
+            }
         }
     }
-
-/*
-********** Code from previous approach below, for reference only
-********** To be deleted in future commit
-*/
-
-struct node {
-    // Make unsigned
-    int size;
-    // Get rid of this. Always go through entire chain of used/free blocks.
-    int magic;
-    // 
-    void *p;
-    struct node *next;
-};
-
-static struct node freeList;
-
-static void malloc_init(void) {
-    void *memory = malloc(128*(1<<20));
-    struct node *fnode = (struct node*)memory;
-    fnode->p = memory + sizeof (struct node);
-    fnode->size = 128 * (1 << 20) - sizeof(struct node);
-    fnode->magic = mmagic;
-    fnode->next = NULL;
-    freeList.next = fnode;
-    freeList.size = 0;
-    freeList.magic = 0;
-    freeList.p = 0;
-    malloc_initd = 1;
-}
-
-void *myMalloc(unsigned int size) {
-    if (malloc_initd == 0) {
-        malloc_init();
+    if (best == NULL) {
+        return NULL;
     }
-    struct node *current = freeList.next;
-    struct node *previous = &freeList;
-    while (current != NULL) {
-        // TODO if p is on an 8-byte boundary, skip this operation.
-        void *p8 = current->p + 8 - (current->p & 7);
-        int usableSize = current->size - (p8 - current->p);
-        if (usableSize == size) {
-            previous->next = current->next;
-            return p8;
-        } else if (usableSize > size ) {
-            void *next = p8 + usableSize;
-            struct node *nextNode = (struct node*)next;
-            nextNode->size = current->size - usableSize;
-            nextNode->magic = mmagic;
-            nextNode->p = next + sizeof(struct node);
-            nextNode->next = current->next;
-            previous->next = nextNode;
-            current->size = usableSize + p8 - current->p;
-            return p8;
-        }
-        current = current->next;
-        previous = previous->next;
-    }
+    // Don't fully understand how this math works.
+    void *mp = best->data + sizeof(dword) - ((uintptr_t)best->data & (sizeof(dword) - 1));
+    // Need to split mp if best->size is gt size
+    return mp;
 }
 
-int main (void) {
-    
-}
 
 void myFree(void *ptr);
 
 int myFreeErrorCode(void *ptr);
+
+int main(void)
+{
+}
+
+
+
